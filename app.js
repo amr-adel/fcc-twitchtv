e947d1f317b1a8c59cd93bb8655cbdb9d2d392b8
Amr A. Abdelmoez <amradel88@live.com>
2018-03-17 14:25:32 +0200

    feat: check stream state

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
-// http://youmightnotneedjquery.com
-const request = new XMLHttpRequest();
-request.open('GET', 'https://wind-bow.glitch.me/twitch-api/streams/ESL_SC2', true);
+const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-        // Success!
-        const data = JSON.parse(this.response);
-        console.log(data.stream.channel.logo);
-    } else {
-        // We reached our target server, but it returned an error
-        console.log('ERROR!!');
-    }
-};
 
-request.onerror = function() {
-    // There was a connection error of some sort
-    console.log('CONNECTION ERROR!!');
-};
+//// http://youmightnotneedjquery.com
+//const request = new XMLHttpRequest();
+//let state = 'online';
+//let type = (state === 'offline' ? 'channels' : 'streams');
+//let name = 'esl_sc2';
+//request.open('GET', 'https://wind-bow.glitch.me/twitch-api/' + type + '/' + name + '/', true);
+//
+//request.onload = function() {
+//    if (this.status >= 200 && this.status < 400) {
+//        // Success!
+//        const data = JSON.parse(this.response);
+//        console.log(data.stream.game);
+//    } else {
+//        // We reached our target server, but it returned an error
+//        console.log('ERROR!!');
+//    }
+//};
+//
+//request.onerror = function() {
+//    // There was a connection error of some sort
+//    console.log('CONNECTION ERROR!!');
+//};
+//
+////request.send();
 
-request.send();
+
+
+channels.forEach(function(channel) {
+    checkState(channel);
+});
+
+
+function checkState(channel) {
+    const checkRequest = new XMLHttpRequest();// http://youmightnotneedjquery.com
+    checkRequest.open('GET', 'https://wind-bow.glitch.me/twitch-api/streams/' + channel + '/', true);
+
+    checkRequest.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            // Success!
+            const data = JSON.parse(this.response);
+            if (data.stream === null) {
+                console.log("offline");
+            } else {
+                console.log("online");
+            }
+        } else {
+            // We reached our target server, but it returned an error
+            console.log('ERROR!!');
+        }
+    };
+
+    checkRequest.onerror = function() {
+        // There was a connection error of some sort
+        console.log('CONNECTION ERROR!!');
+    };
+    
+    checkRequest.send();
+}
