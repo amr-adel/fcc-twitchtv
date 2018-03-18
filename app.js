893efd7cfe53fb4d091323af09cb6fe68d618b55
Amr A. Abdelmoez <amradel88@live.com>
2018-03-18 22:21:23 +0200

    feat: add offline channel data

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
 const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "taketv", "habathcx", "RobotCaleb", "brobson"];
 
+channels.forEach(function(channel) {
+    checkState(channel);
+});
 
-//// http://youmightnotneedjquery.com
-//const request = new XMLHttpRequest();
-//let state = 'online';
-//let type = (state === 'offline' ? 'channels' : 'streams');
-//let name = 'esl_sc2';
-//request.open('GET', 'https://wind-bow.glitch.me/twitch-api/' + type + '/' + name + '/', true);
-//
-//request.onload = function() {
-//    if (this.status >= 200 && this.status < 400) {
-//        // Success!
-//        const data = JSON.parse(this.response);
-//        console.log(data.stream.game);
-//    } else {
-//        // We reached our target server, but it returned an error
-//        console.log('ERROR!!');
-//    }
-//};
-//
-//request.onerror = function() {
-//    // There was a connection error of some sort
-//    console.log('CONNECTION ERROR!!');
-//};
-//
-////request.send();
-
-
-
-function test() {
-    channels.forEach(function(channel) {
-        checkState(channel);
-    });
-}
 
 
 function checkState(channel) {
@@ -44,9 +15,8 @@ function checkState(channel) {
             // Success!
             const data = JSON.parse(this.response);
             if (data.stream === null) {
-                console.log("offline");
+                offline(channel);
             } else {
-                console.log("online");
                 const logo = data.stream.channel.logo;
                 const url = data.stream.channel.url;
                 const name = data.stream.channel.display_name;
@@ -70,11 +40,47 @@ function checkState(channel) {
     checkRequest.send();
 }
 
+
+
 function online(logo, url, name, game, followers, viewers, status) {
-    let card = '<article class="card online"><img src="' + logo + '" class="avatar" alt="' + name + ' avatar"><a href="' + url + '" class="name" target="_blank"><h2 class="namef">' + name + ' <span class="game">' + game + '</span></h2></a><p class="followers">Followers ' + followers + ' • Viewers ' + viewers + '</p><p class="status">' + status + '</p></article>';
+    let card = '<article class="card online"><img src="' + logo + '" class="avatar" alt="' + name + ' avatar"><a href="' + url + '" class="name" target="_blank"><h2>' + name + ' <span class="game">' + game + '</span></h2></a><p class="followers">Followers ' + followers + ' • Viewers ' + viewers + '</p><p class="status">' + status + '</p></article>';
     
     document.getElementById('container').insertAdjacentHTML('beforeend', card)
 }
 
 
 
+function offline(channel) {
+//    console.log(channel + ' is offline.');
+
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://wind-bow.glitch.me/twitch-api/channels/' + channel + '/', true);
+
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            // Success!
+            const data = JSON.parse(this.response);
+            
+            const logo = data.logo;
+            const url = data.url;
+            const name = data.display_name;
+            const followers = data.followers < 1000 ? data.followers : (data.followers / 1000).toFixed(1) + "K";
+            const status = data.status;
+            
+            let card = '<article class="card"><img src="' + logo + '" class="avatar" alt="' + name + ' avatar"><a href="' + url + '" class="name" target="_blank"><h2>' + name + '</h2></a><p class="followers">Followers ' + followers + '</p><p class="status">' + status + '</p></article>';
+
+            document.getElementById('container').insertAdjacentHTML('beforeend', card)
+        } else {
+            // We reached our target server, but it returned an error
+            console.log('ERROR!!');
+        }
+    };
+
+    request.onerror = function() {
+        // There was a connection error of some sort
+        console.log('CONNECTION ERROR!!');
+    };
+
+    request.send();
+    
+}
